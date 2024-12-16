817d35b4b4c5bfaedbcc5d85ca0de5b61a4560cb
Luke 🦃 <mainetout300@gmail.com>
2024-12-16 16:00:20 -0500

    day 1 - part 2

--- day1.py
+++ day1.py
@@ -3,6 +3,30 @@
 def calculate_sorted_list(input_list):
     return sorted(input_list)
 
+def calc_sum(left_list, right_list):
+    new_left = calculate_sorted_list(left_list)
+    new_right = calculate_sorted_list(right_list)
+    sum = 0
+
+    for i in range(len(new_left)):
+        sum += abs(new_right[i] - new_left[i])
+
+    print(sum)
+
+def similarity(left_list, right_list):
+    count = dict()
+    score = 0
+
+    for number in left_list:
+        for other in right_list:
+            times_appeared = 0
+
+            if number == other:
+                times_appeared += 1
+
+            score += number * times_appeared
+
+    print("Similarity score:", score)
 def main():
     left_list, right_list = [], []
 
@@ -19,14 +43,8 @@ def main():
     except FileNotFoundError:
         print("File not found")
 
-    new_left = calculate_sorted_list(left_list)
-    new_right = calculate_sorted_list(right_list)
-    sum = 0
-
-    for i in range(len(new_left)):
-        sum += abs(new_right[i] - new_left[i])
-
-    print(sum)
+    calc_sum(left_list, right_list)
+    similarity(left_list, right_list)
 
 if __name__ == "__main__":
     main()
